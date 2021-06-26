cb9b187cc53b8ab637e9a5016d918b66f793a050
Prakhar Makhija <matcdac@gmail.com>
2021-06-26 17:06:09 +0530

    [RFC] modifying download script

--- docs/download-RFCs-txt.sh
+++ docs/download-RFCs-txt.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# configurable
 directory=txt
+
+# configurable
 target_extension=txt
 cd $directory
+
 pwd
 echo "----------------------------------------------------------------"
 
@@ -18,15 +22,17 @@ do
   echo "parsed rfc number, without leading zeros: $number"
 
   # target is the filename
+  # configurable
   target="rfc$number.$target_extension"
   echo "target file name: $target"
 
   # link is from where the file is to be downloaded
+  # configurable
   link="https://www.rfc-editor.org/rfc/$target"
   echo "link to download from: $link"
 
   # download if target file does not exist
-  if [ -e x.txt ]
+  if [ -e $target ]
     then
       echo "$target exists, skipping"
     else
