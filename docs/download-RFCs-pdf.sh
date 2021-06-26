7fa333725567e88ac74d088f888b3d6521c25158
Prakhar Makhija <matcdac@gmail.com>
2021-06-26 17:18:43 +0530

    [RFC] modifying download scripts

---
+++ docs/download-RFCs-pdf.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# configurable
+directory=pdf
+
+# configurable
+target_extension=txt.pdf
+cd $directory
+
+pwd
+echo "----------------------------------------------------------------"
+
+input="../published-RFC-numbers.txt"
+
+while IFS= read -r line
+do
+  # input line from the file, the RFC number which has been published
+  echo "line from file: $line"
+
+  # number is without leading zeros
+  number=$(echo "$line" | sed 's/^0*//')
+  echo "parsed rfc number, without leading zeros: $number"
+
+  # target is the filename
+  # configurable
+  target="rfc$number.$target_extension"
+  echo "target file name: $target"
+
+  # link is from where the file is to be downloaded
+  # configurable
+  link="https://www.rfc-editor.org/rfc/pdfrfc/$target"
+  echo "link to download from: $link"
+
+  # download if target file does not exist
+  if [ -e $target ]
+    then
+      echo "$target exists, skipping"
+    else
+      echo "$target does not exist, going to download"
+      wget $link
+  fi
+
+  echo "----------------------------------------------------------------"
+done < "$input"
+
+
