8c624c658a42ee453554374a8c7ddac02619556a
Prakhar Makhija <matcdac@gmail.com>
2021-06-26 16:57:49 +0530

    [RFC] modifying download script

--- docs/download-RFCs-txt.sh
+++ docs/download-RFCs-txt.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-input="./published-RFC-numbers.txt"
+directory=txt
+target_extension=txt
+cd $directory
+pwd
+echo "----------------------------------------------------------------"
+
+input="../published-RFC-numbers.txt"
 
 while IFS= read -r line
 do
+  # input line from the file, the RFC number which has been published
   echo "$line"
+
   # number is without leading zeros
   number=$(echo "$line" | sed 's/^0*//')
   echo $number
-  # wget download here
-  echo "----"
+
+  # target is the filename
+  target="rfc$number.$target_extension"
+  echo $target
+
+  # link is from where the file is to be downloaded
+  link="https://www.rfc-editor.org/rfc/$target"
+  echo $link
+
+  # download if target file does not exist
+  if [ -e x.txt ]
+    then
+      echo "$target exists"
+    else
+      echo "$target does not exist"
+      # wget $link
+  fi
+
+  echo "----------------------------------------------------------------"
 done < "$input"
 
 
